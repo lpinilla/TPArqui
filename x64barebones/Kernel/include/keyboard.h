7514a03c0bd1f001232f73169febaf30d6433982
root <root@localhost.localdomain>
2018-10-31 18:58:36 -0300

    .

--- x64barebones/Kernel/include/keyboard.h
+++ x64barebones/Kernel/include/keyboard.h
@@ -3,10 +3,6 @@
 
 #include <string.h>
 
-#define BUFFER_SIZE 80
-
-char buffer[BUFFER_SIZE];
-
 char read_keyboard();
 void erase_buffer();
 void read_from_buffer(char * placeholder, int count);
@@ -33,17 +29,4 @@ int buffer_index = 0; //variable global
 #define MAX_BUFF_SIZE           512
 #define EOF -1
 
-//mapa de caracteres
-unsigned char keycode_map[128] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', /* INDEX: 0 - 9 */
-    '9','0', '-', '=', BACKSPACE, '\t' /* Tab */,'q', 'w', 'e', 'r',	/* INDEX: 10 - 19 */
-  't', 'y', 'u', 'i', 'o', 'p', '[', ']', ENTER_KEY, 0 /* Control */, /* INDEX: 20 - 29 */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* INDEX: 30 - 39 */
- '\'', '`',  LEFT_SHIFT,'\\', 'z', 'x', 'c', 'v', 'b', 'n', /* INDEX: 40 - 49 */
-  'm', ',', '.', '/', RIGHT_SHIFT,'*',0/* Alt */,' '/* Space bar */, CAPS_LOCK, 0 /* F1 */, /* INDEX: 50 - 59 */
-    0/* F2 */,   0/* F3 */,   0/* F4 */,   0/* F5 */,   0/* F6 */,   0/* F7 */,   0/* F8 */,   0/* F9 */, 0 /*F10 */, 0/*Num lock*/, /* INDEX: 60 - 69 */
-    0 /* Scroll Lock */,0 /* Home key */, UP_ARROW /* Up Arrow */, 0 /* Page Up */, '-', LEFT_ARROW/* Left Arrow */, 0, RIGHT_ARROW/* Right Arrow */,'+', 0/*End key*/, /* INDEX: 70 - 79 */
-    DOWN_ARROW/* Down Arrow */,0/* Page Down */, 0/* Insert Key */, 0/* Delete Key */, 0,   0,   0,  0/* F11 Key */, 0/* F12 Key */,
-    0,	/* All other keys are undefined */
-};
 #endif
