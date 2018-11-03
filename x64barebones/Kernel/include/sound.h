3a3cfa1470eac800a2118a97c2c8eae3de3cca25
joaquin <battilanajn@gmail.com>
2018-11-03 14:39:51 -0300

    Arreglado problema de compilacion

--- x64barebones/Kernel/include/sound.h
+++ x64barebones/Kernel/include/sound.h
@@ -4,7 +4,15 @@
 #include <time.h>
 #include <stdint.h>
 
+#define PIT_3_PORT 0x42
+#define PIT_CONFIGURE_PORT 0x43
+#define NOT_KEYBOARD_PORT 0x61
+
+void activate_sound(int frequence_divider);
+void deactivate_sound();
 void beep();
+
+
 
 
 #endif
--- x64barebones/Kernel/sound.c
+++ x64barebones/Kernel/sound.c
@@ -3,11 +3,7 @@
 extern void io_write(uint16_t port, uint8_t data);
 extern uint8_t io_read(uint16_t port);
 
-#define PIT_3_PORT 0x42
-#define PIT_CONFIGURE_PORT 0x43
-#define NOT_KEYBOARD_PORT 0x61
-
-void play_sound(int frequence_div) {
+void activate_sound(int frequence_div) {
  int freq;
  uint8_t read_data;
  uint8_t aux;
@@ -23,13 +19,13 @@ void play_sound(int frequence_div) {
 }
 
 
-void nosound() {
+void deactivate_sound() {
  uint8_t aux = io_read(NOT_KEYBOARD_PORT) & 0xFC;
  io_write(NOT_KEYBOARD_PORT, aux);
 }
 
 void beep() {
-  play_sound(10000);
+  activate_sound(10000);
   time_wait(1);
-  nosound();
+  deactivate_sound();
 }
