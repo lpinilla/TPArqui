e4c273cc354763059bfdfb55918f1714c88715d6
joaquin <battilanajn@gmail.com>
2018-11-03 16:52:01 -0300

    Arreglado problema de mergeo

--- x64barebones/run.sh
+++ x64barebones/run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-qemu-system-x86_64 -soundhw pcspk -hda Image/x64BareBonesImage.qcow2 -m 512 
-=======
+
 qemu-system-x86_64 -soundhw pcspk -hda Image/x64BareBonesImage.qcow2 -m 512
->>>>>>> 97095ae74aa258eb78428828822d7e2c722ea8aa
